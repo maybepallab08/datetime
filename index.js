f4529ed2daa2847520f459cb2505b668212be155
maybepallab08 <77858740+maybepallab08@users.noreply.github.com>
2022-02-24 13:50:35 +0545

    Update index.js

--- index.js
+++ index.js
@@ -5,22 +5,30 @@ function dateshow() {
     month: "long",
     day: "numeric",
   };
+
   const abc = document.querySelector(".date");
   var date = new Date();
   day = date.toLocaleDateString(undefined, options);
   var hours = date.getHours();
   var mins = date.getMinutes();
   var seconds = date.getSeconds();
-  abc.innerHTML = hours + ":" + mins + ":" + seconds + " <br> on " + day;
-
   if (mins < 10) {
     mins = "0" + mins;
   }
   if (seconds < 10) {
     seconds = "0" + seconds;
   }
-  abc.innerHTML = hours + ":" + mins + ":" + seconds + " <br> on " + day;
+  if (hours > 12) {
+    hours = hours - 12;
+    ampm = "PM";
+  } else {
+    ampm = "AM";
+  }
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+  abc.innerHTML =
+    hours + ":" + mins + ":" + seconds + " " + ampm + " <br> on " + day;
 }
-
 dateshow();
 setInterval(dateshow, 1000);
